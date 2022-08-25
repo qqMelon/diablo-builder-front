94727fe58a9ab0b05bec95f82fff9d0989169edd
qqMelon <43541363+qqMelon@users.noreply.github.com>
2022-08-25 17:11:54 +0200

    Create interface of update and mocking data in Dashboard

---
+++ src/interfaces/update.ts
@@ -0,0 +1,8 @@
+export interface Update {
+    id: string
+    title: string
+    imageUrl: string
+    author: string
+    action: string
+    content: string
+}
--- src/views/home-children/Dashboard.vue
+++ src/views/home-children/Dashboard.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import { Set } from "../../interfaces/set";
+import { Set } from "@/interfaces/set";
+import { Update } from "../../interfaces/update";
 import SetCard from '@/components/SetCard.vue'
+import AppList from '@/components/AppList.vue'
+import AppUpdate from '@/components/AppUpdate.vue'
 
 // Mocking data
 const sets: Set[] = [
@@ -8,6 +11,16 @@ const sets: Set[] = [
   { id: '2', title: 'SunWukong Monk', gameVersion: '2.5.7', author: 'qqMelon', classGame: 'Monk', difficulty: 'medium', imageUrl: 'https://eu.diablo3.blizzard.com/static/images/hero/monk/bg.jpg' },
   { id: '3', title: 'Barbstroming', gameVersion: '2.5.7', author: 'qqMelon', classGame: 'Barbarian', difficulty: 'low', imageUrl: 'https://eu.diablo3.blizzard.com/static/images/hero/barbarian/bg.jpg' },
   { id: '4', title: 'Necroman', gameVersion: '2.5.7', author: 'qqMelon', classGame: 'Necromancer', difficulty: 'high', imageUrl: 'https://eu.diablo3.blizzard.com/static/images/hero/necromancer/bg.jpg' }
+]
+
+// Mocking data
+const updates: Update[] = [
+  { id: '1', title: 'Comment', author: 'qqMelon', content: 'Ce build sens le zbouba en vrai c cho', action: 'comment', imageUrl: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
+  { id: '2', title: 'Comment', author: 'qqMelon', content: 'Ce build sens le zbouba en vrai c cho', action: 'comment', imageUrl: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
+  { id: '3', title: 'Update', author: 'qqMelon', content: 'Ce build sens le zbouba en vrai c cho', action: 'comment', imageUrl: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
+  { id: '4', title: 'Comment', author: 'qqMelon', content: 'Ce build sens le zbouba en vrai c cho', action: 'comment', imageUrl: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
+  { id: '5', title: 'Update', author: 'qqMelon', content: 'Ce build sens le zbouba en vrai c cho', action: 'comment', imageUrl: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
+  { id: '6', title: 'Update', author: 'qqMelon', content: 'Ce build sens le zbouba en vrai c cho', action: 'comment', imageUrl: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
 ]
 </script>
 <template>
@@ -23,7 +36,20 @@ const sets: Set[] = [
           </v-col>
         </v-row>
       </v-col>
-      <v-col cols="4"></v-col>
+      <v-col cols="4">
+        <v-row class="pl-3 mb-3">
+          <h5 class="text-h4">Lastest updates</h5>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <AppList lines="2">
+              <template v-for="update in updates" :key="update.id">
+                <AppUpdate :title="update.title" :author="update.author" :image-url="update.imageUrl" :action="update.action" :content="update.content" />
+              </template>
+            </AppList>
+          </v-col>
+        </v-row>
+      </v-col>
     </v-row>
   </v-container>
 </template>
