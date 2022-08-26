4f8b78e23ee8be74327c02d079245bd84739784f
qqMelon <43541363+qqMelon@users.noreply.github.com>
2022-08-26 10:16:47 +0200

    Create link in navbar to navigate in app

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -10,7 +10,25 @@ import AppButton from '@/components/AppButton.vue'
   >
     <v-app-bar-title>Diablo Builder</v-app-bar-title>
     <template v-slot:append>
+      <div class="app-navbar-link-container flex-row align-center">
+        <router-link to="/home-dashboard" class="text-decoration-none text-uppercase mr-4 font-weight-regular">Home</router-link>
+        <router-link to="/home-leaderboard" class="text-decoration-none text-uppercase mr-4 font-weight-regular">LeaderBoard</router-link>
+      </div>
       <AppButton title="Connexion"/>
     </template>
   </v-app-bar>
 </template>
+
+<style scoped>
+.app-navbar-link-container {
+  display: flex;
+}
+
+.app-navbar-link-container > a {
+  color: #B03E3E;
+}
+
+.app-navbar-link-container > a:hover {
+  color: #29292C;
+}
+</style>
