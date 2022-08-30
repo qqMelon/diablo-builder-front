97e73fe8f3bfd77a1c644a70954b2d8259e915a6
Maxime NICOLAS <maxime.nicolas@conserto.pro>
2022-08-30 11:58:25 +0200

    Call in app component parent mounted hook

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <script lang="ts">
-import { useTheme } from 'vuetify'
+import {useTheme} from 'vuetify'
+import Client from "./services/client";
+import { useClientStore } from "./stores/client";
 
 export default {
+  name: 'App',
+  methods: {
+    initAppWithAccessToken: async function () {
+      const clientStore = useClientStore()
+      const req = await Client.getAccessToken()
+      const reqForm = {
+        accessToken: req.access_token as string,
+        expireIn: req.expires_in as number
+      }
+      clientStore.credentials = reqForm as any
+      localStorage.setItem('diabloBuilderAccessToken', reqForm.accessToken as string)
+      localStorage.setItem('diabloBuilderExpireToken', reqForm['expireIn'].toString())
+    }
+  },
+  mounted: function () {
+    const clientStore = useClientStore()
+    if (clientStore.credentials.accessToken !== '') {
+      this.initAppWithAccessToken()
+    }
+  },
   setup: function () {
     const theme = useTheme()
 
