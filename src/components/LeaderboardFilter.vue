8f0f5963d65f8b9052e2d32ebaed00ed6c9c600b
Maxime NICOLAS <maxime.nicolas@conserto.pro>
2022-08-30 17:21:38 +0200

    Copy and change component from SetFilter for leaderBoard

---
+++ src/components/LeaderboardFilter.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+const props = defineProps<{
+  classChoice: String,
+  modChoice: String,
+  typeChoice: String
+}>()
+
+const itemsClass = [ 'Barbarian', 'Witchdoctor', 'Demon hunter', 'Monk', 'Wizzard', 'Necromancer', 'Crusader' ]
+const itemsMod = [ 'Normal', 'Seasonal', 'Hardcore' ]
+const itemsType = [ 'Achievement Point', 'Rift' ]
+
+const searchByClass = ref<string>()
+const searchByMod = ref<string>()
+const searchByType = ref<string>()
+
+onMounted(() => {
+  searchByClass.value = props.classChoice
+  searchByMod.value = props.modChoice
+  searchByType.value = props.typeChoice
+})
+
+// Rules input id needed
+// :rules="[v => !!v || 'Item is required']"
+</script>
+
+<template>
+  <v-select
+      class="app-set-filter mr-3"
+      v-model="searchByClass"
+      :items="itemsClass"
+      label="By Class"
+      variant="outlined"
+      required
+  ></v-select>
+  <v-select
+      class="app-set-filter mr-3"
+      v-model="searchByMod"
+      :items="itemsMod"
+      label="Mod"
+      variant="outlined"
+      required
+  ></v-select>
+  <v-select
+      class="app-set-filter"
+      v-model="searchByType"
+      :items="itemsType"
+      label="Type"
+      variant="outlined"
+      required
+  ></v-select>
+</template>
+
+<style scoped>
+.app-set-filter {
+  max-width: 187px;
+}
+.app-set-filter.set-filter-tag {
+  min-width: 187px;
+  max-width: max-content;
+}
+</style>
