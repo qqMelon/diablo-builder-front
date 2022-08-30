ac455296eccf68a8427744641eab837eaecf863b
Maxime NICOLAS <maxime.nicolas@conserto.pro>
2022-08-30 11:57:19 +0200

    Temporary comment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
 import AppNavbar from '@/components/AppNavbar.vue'
-import http from "@/services";
-import { onUpdated } from "vue";
-import { useSeasonStore } from '@/stores/season'
+// import http from "@/services";
+import { onMounted } from "vue";
+// import { useSeasonStore } from '@/stores/season'
 
-const seasonStore = useSeasonStore()
+// const seasonStore = useSeasonStore()
 
 // Get and store the active season
-const getActiveSeason = function () {
-  http.get('/data/d3/season/')
-      .then((response: any) => {
-        seasonStore.currentSeason = response.data.current_season
-      })
-      .catch((error: any) => { console.warn(error)})
-}
+// const getActiveSeason = function () {
+//   http.get('/data/d3/season/')
+//       .then((response: any) => {
+//         seasonStore.currentSeason = response.data.current_season
+//       })
+//       .catch((error: any) => { console.warn(error)})
+// }
 
-onUpdated(() => {
-  getActiveSeason()
+onMounted(() => {
+  // getActiveSeason()
 })
 </script>
 
