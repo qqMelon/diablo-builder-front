2ae6415e932354fe0f1608665d2f243cb72897d0
Maxime NICOLAS <maxime.nicolas@conserto.pro>
2022-08-30 11:58:05 +0200

    Setup store

--- src/stores/client.ts
+++ src/stores/client.ts
@@ -2,9 +2,9 @@ import { defineStore } from "pinia";
 
 export const useClientStore = defineStore('client', {
     state: () => ({
-        tokenSecret: import.meta.env.VITE_BLIZZ_API_KEY,
-        tokenClient: import.meta.env.VITE_BLIZZ_CLIENT_ID,
-        accessToken: import.meta.env.VITE_ACCESS_TOKEN,
-        baseUrl: 'https://eu.api.blizzard.com/'
+        credentials: {
+            accessToken: localStorage.getItem('diabloBuilderAccessToken') || '',
+            expireIn: localStorage.getItem('diabloBuilderExpireToken') ||  null
+        }
     })
 })
